b496872373fd04bbeea82eb0046bbf7f9a179a85
som40693@gmail.com <som40693@gmail.com>
2021-12-27 05:36:17 +0200

    Added source code files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8.0-buster
+
+WORKDIR /application
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY /app .
+
+
+CMD ["python", "project.py"]
---
+++ app/project.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+class Team :
+    def __init__(self , name , sec , department ):
+        self.name = name
+        self.sec = sec
+        self.department= department
+        
+        
+def members ():
+    member1 = Team("Ahmed Abd El-Maksoud Ahmed " , 4 , "CS")
+    member2 = Team("Hazem Ahmad Ahmad Rady" , 2 , "CS") 
+    member3 = Team("Mostafa Eid Abd El-Khalek " , 4 , "CS")
+    member4 = Team("Hadeer Kandeel Abd El-Ghaffar " , 4 , "CS")
+    member5 = Team("Yasmin Muhammad Abdullah" , 4 , "CS")
+    
+    print("Team members ")
+    
+    print("1 ")
+    print(member1.name , "    sec : " , member1.sec , "  " , member1.department)
+    
+    print("2 ")
+    print(member2.name , "         sec : " , member2.sec , "  " , member3.department)
+
+    print("3 ")
+    print(member3.name , "     sec : " , member3.sec , "  " , member3.department)
+
+    print("4 ")
+    print(member4.name , "  sec : " , member4.sec , "  " , member4.department)
+
+    print("5 ")
+    print(member5.name , "        sec : " , member5.sec , "  " , member5.department)
+
+print("")
+members()
+app.run(host='0.0.0.0', port=5000)
